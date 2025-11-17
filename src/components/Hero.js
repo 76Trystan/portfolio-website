04501e4922490d7295c86882b4a1333acf79703c
Trystan <trystan.frederick@gmail.com>
2025-11-17 18:51:54 +1100

    read-added hero

---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section className="hero" id="hero">
+      <h1>Trystan</h1>
+      <p>Software Engineering • FullStack Developer • Problem Solver</p>
+    </section>
+  );
+}
+
+export default Hero;
