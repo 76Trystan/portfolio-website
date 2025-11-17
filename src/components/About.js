37f48a57b4392070ff0599ad87e738320ef64a5d
Trystan <trystan.frederick@gmail.com>
2025-11-17 18:28:29 +1100

    component update

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        Will add content about myself here soon.
+      </p>
+    </section>
+  );
+}
+
+export default About;
