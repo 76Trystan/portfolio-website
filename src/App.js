ac31201e163bebb3e9740184b1bf1bfb6aa40259
Trystan <trystan.frederick@gmail.com>
2025-11-17 18:24:12 +1100

    added components for website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>My website starts here.</p>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
