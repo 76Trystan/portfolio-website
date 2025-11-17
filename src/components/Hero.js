9d7dccc74686145218008b8ac0e59d9a0a0d537c
Trystan <trystan.frederick@gmail.com>
2025-11-17 19:03:29 +1100

    updated hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <section className="hero fadein" id="hero">
-      <h1>Trystan</h1>
+      <h1>Trystan Frederick</h1>
       <p>Software Engineering • FullStack Developer • Problem Solver</p>
     </section>
   );
