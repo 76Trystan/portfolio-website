8cae74ee5c74d856fa7fbee606783b6ee6884f2d
Trystan <trystan.frederick@gmail.com>
2025-11-17 19:02:43 +1100

    update style sheet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,10 @@
 function Contact() {
   return (
-    <section id="contact">
+    <section className="fade-in" id="contact">
       <h2>Contact Me</h2>
-      <p>Email: trystan.frederick@gmail.com</p>
-      <p>LinkedIn: www.linkedin.com/in/trystan-frederick</p>
-      <p>GitHub: https://github.com/76Trystan</p>
+      <p>Email: <a href="mailto:trystan.frederick@gmail.com">trystan.frederick@gmail.com</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/trystan-frederick/" target="_blank">https://www.linkedin.com/in/trystan-frederick/</a></p>
+      <p>GitHub: <a href="https://github.com/76Trystan" target="_blank">https://github.com/76Trystan</a></p>
     </section>
   );
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 function Hero() {
   return (
-    <section className="hero" id="hero">
+    <section className="hero fadein" id="hero">
       <h1>Trystan</h1>
       <p>Software Engineering • FullStack Developer • Problem Solver</p>
     </section>
